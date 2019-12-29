e98d9fcafbcc57a6f0185d70a39282f483b00473
Michał Startek <michal.startek.uw@gmail.com>
2019-12-29 02:03:02 +0100

    Fix exp

--- sampling/beta-ordered.py
+++ sampling/beta-ordered.py
@@ -21,12 +21,10 @@ def ordered_unif_seq(samplesize):
 def ordered_unif_exp(samplesize):
     if samplesize <= 0:
         return
-    csum = -log(uniform(0.0, 1.0))
+    L = [-log(uniform(0.0, 1.0))]
     for _unused in xrange(samplesize):
-        expv = -log(uniform(0.0, 1.0))
-        next_csum = csum + expv
-        print(csum, next_csum, csum/next_csum)
-        yield csum/next_csum
-        csum = next_csum
+        L.append(L[-1] - log(uniform(0.0, 1.0)))
+    last = L.pop()
+    return [x/last for x in L]
 
 print(list(ordered_unif_exp(100)))
