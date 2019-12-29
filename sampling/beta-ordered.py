059e9c83968c9efbf8501a27be7a7de3d3490298
Michał Startek <michal.startek.uw@gmail.com>
2019-12-29 13:58:29 +0100

    More fun

--- sampling/beta-ordered.py
+++ sampling/beta-ordered.py
@@ -12,10 +12,10 @@ def ordered_unif_seq(samplesize):
     if samplesize <= 0:
         return
     last = uniform(0.0, 1.0)**(1.0/samplesize)
-    yield last
+    yield 1.0-last
     for n in xrange(samplesize-1, 0, -1):
         last = last * uniform(0.0, 1.0)**(1.0/n)
-        yield last
+        yield 1.0-last
 
 
 def ordered_unif_exp(samplesize):
@@ -27,4 +27,29 @@ def ordered_unif_exp(samplesize):
     last = L.pop()
     return [x/last for x in L]
 
-print(list(ordered_unif_exp(100)))
+
+
+def _beta_1_b(b):
+    '''Returns a random variate from beta(1, b) distribution
+    using the inverse CDF method'''
+    return 1.0-uniform(0.0, 1.0)**(1.0/b)
+
+
+def ordered_unif_beta(samplesize):
+    now = 0.0
+    for n in xrange(samplesize-1, 0, -1):
+        B = uniform(0.0, 1.0)**(1.0/n)
+        now = 1.0 - B*(1.0 - now)
+        yield now
+
+def ordered_unif_seq_rev(samplesize):
+    if samplesize <= 0:
+        return
+    last = uniform(0.0, 1.0)**(1.0/samplesize)
+    yield last
+    for n in xrange(samplesize-1, 0, -1):
+        last = last * uniform(0.0, 1.0)**(1.0/n)
+        yield last
+
+
+print(list(ordered_unif_seq(100)))
